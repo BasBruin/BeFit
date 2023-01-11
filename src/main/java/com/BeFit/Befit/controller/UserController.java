69b7e2e40ff48f55a1736cca5ff0e5295e205767
Bas Bruin <487790@student.fontys.nl>
2023-01-11 15:27:32 +0100

    Create gefixt

--- src/main/java/com/BeFit/Befit/controller/UserController.java
+++ src/main/java/com/BeFit/Befit/controller/UserController.java
@@ -2,8 +2,12 @@ package com.BeFit.Befit.controller;
 
 import com.BeFit.Befit.model.User;
 import com.BeFit.Befit.repository.UserRepository;
+import netscape.javascript.JSObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -11,13 +15,21 @@ import java.util.List;
 @CrossOrigin
 @RequestMapping("/Gymmember")
 public class UserController {
-
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/add")
-    User newUser(@RequestBody User gymMember){
-        return userRepository.save(gymMember);
+    User newUser(@RequestBody String newUser) throws JSONException {
+        final JSONObject obj = new JSONObject(newUser);
+        String username = obj.getString("Name");
+        int prBench = Integer.parseInt(obj.getString("PRBench"));
+
+        User _newUser = new User();
+        _newUser.setName(username);
+        _newUser.setPRBench(prBench);
+
+        return userRepository.save(_newUser);
+
     }
 
     @GetMapping("/getAll")
